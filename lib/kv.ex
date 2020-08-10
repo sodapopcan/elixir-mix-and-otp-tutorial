37f9442e4c51dbacbf90b95524123d4077f27ae9
Andrew Haust <andrewwhhaust@gmail.com>
2020-08-10 08:04:20 -0400

    Start supervisor when starting the application

--- lib/kv.ex
+++ lib/kv.ex
@@ -1,5 +1,10 @@
 defmodule KV do
-  def hello do
-    :world
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    # Although we don't use the supervisor name below directly,
+    # it can be useful when debugging or introspecting the system.
+    KV.Supervisor.start_link(name: KV.Supervisor)
   end
 end
